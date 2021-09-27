1afb83fb4ade35676f7ced59a2cb3dd894013c33
Aren Villanueva <kurikaesu@users.noreply.github.com>
2021-09-27 10:44:51 -0700

    Add resolution to the Artist 22E Pro attributes

--- src/artist_22e_pro.cpp
+++ src/artist_22e_pro.cpp
@@ -109,6 +109,7 @@ bool artist_22e_pro::attachDevice(libusb_device_handle *handle, int interfaceId)
     int maxWidth = (buf[3] << 8) + buf[2];
     int maxHeight = (buf[5] << 8) + buf[4];
     int maxPressure = (buf[9] << 8) + buf[8];
+    int resolution = (buf[11] << 8) + buf[10];
 
     unsigned short vendorId = 0x28bd;
     unsigned short productId = 0xf90b;
@@ -118,6 +119,7 @@ bool artist_22e_pro::attachDevice(libusb_device_handle *handle, int interfaceId)
         .maxWidth = maxWidth,
         .maxHeight = maxHeight,
         .maxPressure = maxPressure,
+        .resolution = resolution,
         .maxTiltX = 60,
         .maxTiltY = 60,
         .vendorId = vendorId,
